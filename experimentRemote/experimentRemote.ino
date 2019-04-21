8b274420de279a8ff2472cb825789975ffcdd8cd
jeremysavonet <jeremy.savonet@gmail.com>
2019-04-21 17:25:47 +0200

    Experiment on Wemos D1

--- experimentRemote/experimentRemote.ino
+++ experimentRemote/experimentRemote.ino
@@ -2,63 +2,51 @@
 // Copyright (c) 2019 All Rights Reserved WestBot
 
 #include <FastLED.h>
+#include <Stepper.h>
 #include <SoftwareSerial.h>
 
 // USER DEFINE
-#define LED_PIN     5
-#define NUM_LEDS    6
+#define DATA_PIN    D1
+#define NUM_LEDS    14
 #define BRIGHTNESS  255
 #define LED_TYPE    WS2811
 #define COLOR_ORDER GRB
 
-#define enA 9
-#define in1 6
-#define in2 7
-#define ATmodePin 4
-#define LedPin 5
-#define PwmOutput 255 // FULL SPEED NOW
-
 #define RUNNING_FOR 30000L // 30 secondes
 #define UPDATES_PER_SECOND 100
 
-CRGB leds[NUM_LEDS];
+CRGB leds[ NUM_LEDS ];
+CRGB color( 255, 0, 0 );
 
-SoftwareSerial BTserial( 2, 3 );
- 
+SoftwareSerial BTserial( D2, D3 );
+
+const int stepsPerRevolution = 200; // change this to fit the number of steps per revolution
+int stepCount = 0; // number of steps the motor has taken
+//const int stepsPerRevolution = 2048; //2048 pour la 28BYJ-48 = 1 tour
+int Nbre_tour = 0;
+
+// initialize the stepper library on pins D5 to D8
+Stepper myStepper( stepsPerRevolution, D6, D8, D7, D5 );
+
 char c = ' ';
- 
+
 void setup() 
 {
-    // Init LEDs
-    FastLED.addLeds<LED_TYPE, LED_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
-    FastLED.setBrightness(  BRIGHTNESS );
+    myStepper.setSpeed( 100 ); // de 4 a 14 vitesse de rotation
 
-    // Static red for QR
-    leds[0] = CRGB::Yellow; 
-    leds[1] = CRGB::Yellow;
-    leds[2] = CRGB::Yellow;
-    leds[3] = CRGB::Yellow;
-    leds[4] = CRGB::Yellow;
-    leds[5] = CRGB::Yellow; 
+    // Init LEDs
+    FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection( TypicalLEDStrip );
+    FastLED.setBrightness(  BRIGHTNESS );
+  
+    for( int i = 0; i < NUM_LEDS; ++i )
+    {
+        leds[ i ] = CRGB::Black;
+    }
 
     FastLED.show();
-
-    // Configure pin for PWM and motor control
-    pinMode( enA, OUTPUT );
-    pinMode( in1, OUTPUT );
-    pinMode( in2, OUTPUT );
     
-    // Set initial rotation direction
-    digitalWrite( in1, LOW );
-    digitalWrite( in2, HIGH );
-  
     // Configure the blinking led pin
-    pinMode( LedPin, OUTPUT ); 
-    pinMode(LED_BUILTIN, OUTPUT);
-
-    // Configure pin to enter AT mode
-    pinMode( ATmodePin, OUTPUT ); 
-    digitalWrite( ATmodePin, LOW ); 
+    pinMode( LED_BUILTIN, OUTPUT );
  
     Serial.begin( 9600 );
     Serial.println("Welcome bitches");
@@ -80,52 +68,52 @@ void loop()
         {
             Serial.println( "Starting experience" );
 
-            analogWrite( enA, PwmOutput ); // Send PWM signal to L298N Enable pin
 
             // Set PWM until reaching top
             for( uint32_t tStart = millis();  ( millis() - tStart ) < RUNNING_FOR; )
             {
-              leds[0] = CRGB::Red; 
-              leds[1] = CRGB::Red;
-              leds[2] = CRGB::Red;
-              leds[3] = CRGB::Red;
-              leds[4] = CRGB::Red;
-              leds[5] = CRGB::Red; 
-              FastLED.show();
-              delay( 500 );
-              leds[0] = CRGB::Black; 
-              leds[1] = CRGB::Black;
-              leds[2] = CRGB::Black;
-              leds[3] = CRGB::Black;
-              leds[4] = CRGB::Black;
-              leds[5] = CRGB::Black; 
-              FastLED.show();
-              delay( 500 );              
+              myStepper.step( 300 );  
+              for( int i = 0; i < NUM_LEDS; ++i )
+              {
+                leds[ i ] = CRGB::Red;
+              }
+              FastLED.show();   
+              delay( 100 );      
+              
+              for( int i = 0; i < NUM_LEDS; ++i )
+              {
+                leds[ i ] = CRGB::Black;
+              }
+              FastLED.show();   
+              delay( 100 ); 
             }
-
-            analogWrite( enA, 0 ); // STOP PWM
+            
             // END EXPERIENCE
-        }
-        
-        if( c == '0' )
-        {
-            leds[0] = CRGB::Purple; 
-            leds[1] = CRGB::Purple;
-            leds[2] = CRGB::Purple;
-            leds[3] = CRGB::Purple;
-            leds[4] = CRGB::Purple;
-            leds[5] = CRGB::Purple; 
+
+            for( int i = 0; i < NUM_LEDS; ++i )
+            {
+                leds[ i ] = color;
+            }
             FastLED.show();
         }
-        
-        if( c == '1' )
+        else if( c == '0' )
         {
-            leds[0] = CRGB::Yellow; 
-            leds[1] = CRGB::Yellow;
-            leds[2] = CRGB::Yellow;
-            leds[3] = CRGB::Yellow;
-            leds[4] = CRGB::Yellow;
-            leds[5] = CRGB::Yellow; 
+            for( int i = 0; i < NUM_LEDS; ++i )
+            {
+                leds[ i ] = CRGB::Purple;
+            }
+
+            color = CRGB::Purple;
+            FastLED.show();
+        }
+        else if( c == '1' )
+        {
+            for( int i = 0; i < NUM_LEDS; ++i )
+            {
+                leds[ i ] = CRGB::Yellow;
+            } 
+
+            color = CRGB::Yellow;
             FastLED.show();
         }
     }
@@ -136,12 +124,10 @@ void loop()
  
         if( c=='#' )  
         {
-            digitalWrite( ATmodePin, HIGH );
             Serial.print( "Entering configuration mode (type '&' to exit)" );
         }
         else if( c == '$' ) 
         {
-            digitalWrite( ATmodePin, LOW );
             BTserial.print( "AT+RESET\n\r" );
             Serial.print( "AT+RESET\n\r" ); 
         }        
